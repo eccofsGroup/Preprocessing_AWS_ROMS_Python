54080c2c4c5d4fd81f4a26e80b922188fa868c09
eccofsGroup <149108469+eccofsGroup@users.noreply.github.com>
2023-10-27 11:10:32 -0400

    Update run_aws_instance.sh

--- run_aws_instance.sh
+++ run_aws_instance.sh
@@ -2,7 +2,7 @@
 
 aws ec2 start-instances --instance-ids i-0becb62354fcacf73
 
-ssh -i "oceanmodeling.pem" ubuntu@ec2-44-209-181-39.compute-1.amazonaws.com 'python3 mainCodeROMSv3.py'
+ssh -i "oceanmodeling.pem" ubuntu@ec2-44-209-181-39.compute-1.amazonaws.com 'python3 mainCodeRoms.py'
 scp -i "oceanmodeling.pem" ubuntu@ec2-44-209-181-39.compute-1.amazonaws.com:/home/ubuntu/urlDownloads.txt .
 bash urlDownloads.txt
 
