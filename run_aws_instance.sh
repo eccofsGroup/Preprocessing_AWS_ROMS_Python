75673e82f50f16019e571530c3e896663e2b972a
eccofsGroup <149108469+eccofsGroup@users.noreply.github.com>
2023-10-27 11:10:02 -0400

    Update run_aws_instance.sh

--- run_aws_instance.sh
+++ run_aws_instance.sh
@@ -6,4 +6,4 @@ ssh -i "oceanmodeling.pem" ubuntu@ec2-44-209-181-39.compute-1.amazonaws.com 'pyt
 scp -i "oceanmodeling.pem" ubuntu@ec2-44-209-181-39.compute-1.amazonaws.com:/home/ubuntu/urlDownloads.txt .
 bash urlDownloads.txt
 
-#aws ec2 stop-instances --instance-ids i-0becb62354fcacf73
+aws ec2 stop-instances --instance-ids i-0becb62354fcacf73
